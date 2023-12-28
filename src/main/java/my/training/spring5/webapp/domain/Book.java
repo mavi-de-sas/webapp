c6bee2365a612875fee5e279fee33685f8b7f775
Serhii Pankiv <mavi.de.sas@gmail.com>
2023-12-28 18:17:06 +0200

    controllers

--- src/main/java/my/training/spring5/webapp/domain/Book.java
+++ src/main/java/my/training/spring5/webapp/domain/Book.java
@@ -78,7 +78,6 @@ public class Book {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
     
